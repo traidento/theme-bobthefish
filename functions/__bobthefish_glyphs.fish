a452d1fccc4a15e23b05076737bfde295de4aac3
Zhai2333 <im@zhai2333.xyz>
2019-03-15 21:32:02 +0800

    [*] fix: Non-zero exit padding

--- functions/__bobthefish_glyphs.fish
+++ functions/__bobthefish_glyphs.fish
@@ -10,7 +10,7 @@ function __bobthefish_glyphs -S -d 'Define glyphs used by bobthefish'
   # Additional glyphs
   set -x detached_glyph          \u27A6
   set -x tag_glyph               \u2302
-  set -x nonzero_exit_glyph      \uFAD5\ $last_status
+  set -x nonzero_exit_glyph      \uFAD5$last_status\ 
   set -x superuser_glyph         \uF922
   set -x bg_job_glyph            \uF53E\  
   set -x hg_glyph                \u263F
